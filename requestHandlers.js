95c4df25a8c3e813b05714493aec74fffbab23dc
joelpdurham <joeldur@gmail.com>
2019-11-30 13:29:49 -0800

    the wrong way to do it

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,19 @@
+const exec = require('child_process').exec;
+
 function start() {
-    console.log('Request handler start was called.');   
+    console.log('Request handler start was called.');
+    let content = 'empty';
+
+    exec('ls -lah', function(error, stdout, stderr) {
+        content = stdout;
+    });
+
+    return content;
 }
 
 function upload() {
     console.log('Rquest handler upload was called');   
+    return 'Hello Upload';
 }
 
 exports.start = start;
--- server.js
+++ server.js
@@ -5,11 +5,10 @@ function start(route, handle) {
     function onRequest(request, response) {
         const pathname = url.parse(request.url).pathname;
         console.log(`Request for ${pathname} recevied.`);
-
-        route(handle, pathname);
         
         response.writeHead(200, { 'Content-Type': 'text/plain' });
-        response.write('Hello World');
+        const content = route(handle, pathname);
+        response.write(content);
         response.end();
     }
     
