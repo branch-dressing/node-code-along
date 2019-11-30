b7ef03d9052a220015aa64d73adfedbb424d0281
joelpdurham <joeldur@gmail.com>
2019-11-29 20:47:01 -0800

    started routes

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const http = require('http');
 
-function onRequest(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('Hello World');
-    response.end();
+function start() {
+    function onRequest(request, response) {
+        console.log('Request recevied.');
+        response.writeHead(200, { 'Content-Type': 'text/plain' });
+        response.write('Hello World');
+        response.end();
+    }
+    
+    http.createServer(onRequest).listen(8888);
+    console.log('Server has started.');
 }
 
-http.createServer(onRequest).listen(8888);
+exports.start = start;
+
